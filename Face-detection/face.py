2312369a0da491ca94a78780bc67ed62f284d026
dreamjh20 <dreamjh20@gmail.com>
2021-09-13 11:31:15 +0900

    Set: Comment

--- Face-detection/face.py
+++ Face-detection/face.py
@@ -5,11 +5,10 @@ cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1600)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 900)
 
-while True: # 무한 루프
-    
+while True:    
     ret, frame = cap.read()
     cv2.imshow("My Sunflower", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):  #q to quit
         break
 
 cap.release()
