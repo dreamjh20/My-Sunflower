6e171c7752bba54f3ac007e335c30f8a15d5ad58
dreamjh20 <dreamjh20@gmail.com>
2021-09-14 09:57:15 +0900

    CREATE: Idea

--- Servo/servo.ino
+++ Servo/servo.ino
@@ -1,9 +1,29 @@
+#define LED 13
+
+int input_data;
+
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED,LOW);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  while(Serial.available())
+  {
+    input_data = Serial.read();
+  }
 
+  if(input_data == '1')
+  {
+    digitalWrite(LED, HIGH); // led_on
+  }
+  else if(input_data == '0')
+  {
+    digitalWrite(LED, LOW); // led_off
+  }
 }
